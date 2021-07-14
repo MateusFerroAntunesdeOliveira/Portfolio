5f33d6103ac0f940a1ff871b59ac0b8f66028282
Mateus <mateus.ferro.2001@hotmail.com>
2021-07-14 14:14:47 -0300

    Remove test folder and update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'src/utils.dart';
 
 import 'package:get/get_core/src/get_main.dart';
-import 'package:myownwebsite/src/services/navigation.service.dart';
 
 import 'src/app.dart';
+import 'src/utils.dart';
+import 'src/services/navigation.service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
