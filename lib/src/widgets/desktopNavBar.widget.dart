be3dfb7f6d142d6b37e844a5c6f02bab1db1f26e
Mateus <mateus.ferro.2001@hotmail.com>
2021-08-09 18:05:31 -0300

    Text Under Development

--- lib/src/widgets/desktopNavBar.widget.dart
+++ lib/src/widgets/desktopNavBar.widget.dart
@@ -8,42 +8,48 @@ class DesktopNavBar extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 48),
       child: Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Mateus Ferro Antunes de Oliveira",
-              style: MfaoTextStyles.title.withColor(MfaoColors.dark),
-            ),
-            Row(
-              children: [
-                //   SizedBox(width: 32),
-                //   Text("Home", style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark)),
-                //   SizedBox(width: 32),
-                //   Text("About Us", style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark)),
-                //   SizedBox(width: 32),
-                //   Text("Portfolio", style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark)),
-                //   SizedBox(width: 32),
-                Container(
-                  width: 200,
-                  child: ElevatedButton(
-                    style: BasicButtonStyle(
-                      backgroundColor: MfaoColors.lightDark,
-                      shadowColor: MfaoColors.dark,
-                      elevation: 2,
-                    ),
-                    onPressed: () {},
-                    child: Text(
-                      "Home",
-                      style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 32),
-              ],
-            )
-          ],
+        child: Center(
+          child: Text(
+            "UNDER DEVELOPMENT",
+            style: MfaoTextStyles.title.withColor(MfaoColors.dark).copyWith(fontSize: 56),
+          ),
         ),
+        // child: Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   children: [
+        //     // Text(
+        //     //   "Mateus Ferro Antunes de Oliveira",
+        //     //   style: MfaoTextStyles.title.withColor(MfaoColors.dark),
+        //     // ),
+        //     // Row(
+        //     //   children: [
+        //     //     //   SizedBox(width: 32),
+        //     //     //   Text("Home", style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark)),
+        //     //     //   SizedBox(width: 32),
+        //     //     //   Text("About Us", style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark)),
+        //     //     //   SizedBox(width: 32),
+        //     //     //   Text("Portfolio", style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark)),
+        //     //     //   SizedBox(width: 32),
+        //     //     Container(
+        //     //       width: 200,
+        //     //       child: ElevatedButton(
+        //     //         style: BasicButtonStyle(
+        //     //           backgroundColor: MfaoColors.lightDark,
+        //     //           shadowColor: MfaoColors.dark,
+        //     //           elevation: 2,
+        //     //         ),
+        //     //         onPressed: () {},
+        //     //         child: Text(
+        //     //           "Home",
+        //     //           style: MfaoTextStyles.subtitle1.withColor(MfaoColors.dark),
+        //     //         ),
+        //     //       ),
+        //     //     ),
+        //     //     SizedBox(width: 32),
+        //     //   ],
+        //     // ),
+        //   ],
+        // ),
       ),
     );
   }
